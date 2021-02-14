8d3c15fdfa7526802c6ad7d841924f7f6dfc553f
quickpoint <52743996+ItIjH7frYg@users.noreply.github.com>
2021-02-14 17:13:13 +0100

    ready for chapter 3

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include "vec3.h"
 
 int main() {
 	std::ofstream myfile;
@@ -10,14 +11,13 @@ int main() {
 	
 	myfile << "P3\n" << nx << " " << ny << "\n255\n";
 	
-	for (int j = ny-1; j >= 0; j--) {
-		for (int i = 0; i < nx; i++) {
-			float r = float(i) / float(nx);
-			float g = float(j) / float(ny);
-			float b = 0.2;
-			int ir = int(255.9*r);
-			int ig = int(255.9*g);
-			int ib = int(255.9*b);
+	for ( int j = ny-1; j >= 0; j-- ) {
+		for ( int i = 0; i < nx; i++ ) {
+			vec3 col( float( i ) / float( nx ), float( j ) / float( ny ), 0.2 );
+			int ir = int( 255.99 * col[ 0 ] );
+			int ig = int( 255.99 * col[ 1 ] );
+			int ib = int( 255.99 * col[ 2 ] );
+			
 			myfile << ir << " " << ig << " " << ib << "\n";
 		}
 	}
