6c3ce56dc1e7210993096fb78c89a0d0bb3d422f
quickpoint <52743996+ItIjH7frYg@users.noreply.github.com>
2021-02-14 19:15:43 +0100

    made shit

--- main-2.cpp
+++ main-2.cpp
@@ -2,12 +2,7 @@
 #include <fstream>
 // #include "vec3.h"
 #include "ray.h"
-
-vec3 color(const ray& r) {
-	vec3 unit_direction = unit_vector(r.direction());
-	float t = 0.5*(unit_direction.y() + 1.0);
-	return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
-}
+#include "operations.h"
 
 int main() {
 	std::ofstream myfile;
@@ -27,14 +22,44 @@ int main() {
 		for ( int i = 0; i < nx; i++ ) {
 			float u = float(i) / float(nx);
 			float v = float(j) / float(ny);
+
+			// adding a gradient background
 			ray r(origin, lower_left_corner + u*horizontal + v*vertical);
 			vec3 col = color(r);
+			
 			int ir = int( 255.99 * col[ 0 ] );
 			int ig = int( 255.99 * col[ 1 ] );
 			int ib = int( 255.99 * col[ 2 ] );
+
+			// coloring red any pixel that hits a small sphere we place at -1 on the z-axis
 			
 			myfile << ir << " " << ig << " " << ib << "\n";
 		}
 	}
+
 	myfile.close();
+}
+
+float hit_sphere(const vec3& center, float radius, const ray& r) {
+	vec3 oc = r.origin() - center;
+	float a = dot(r.direction(), r.direction());
+	float b = 2.0 * dot(oc, r.direction());
+	float c = dot(oc, oc) - radius*radius;
+	float discriminant = b*b - 4*a*c;
+	if (discriminant < 0) {
+		return -1.0;
+	} else {
+		return (-b - sqrt(discriminant)) / (2.0*a);
+	}
+}
+
+vec3 color(const ray& r) {
+	float t = hit_sphere(vec3(0,0,-1), 0.5, r);
+	if (t > 0.0) {
+		vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0,0,-1));
+		return 0.5*vec3(N.x()+1, N.y()+1, N.z()+1);
+	}
+	vec3 unit_direction = unit_vector(r.direction());
+	t = 0.5*(unit_direction.y() + 1.0);
+	return (1.0-t)*vec3(1.0,1.0,1.0) + t*(0.5,0.7,1.0);
 }
